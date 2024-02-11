bb17f587dad3b1ce01963e80a6f6c1aa9c68d348
TLtlbo <js1436kt@gmail.com>
2024-02-12 00:51:15 +0900

    파이어베이스 realTimeDatabase에 유저를 추가하는 로직 추가 로그인 로직에서 새로운 유저가 로그인 되면 해당 유저의 정보 객체를 JSONEncoder로 인코딩한 후 jsonObject로 변환하여 DB에 User 정보를 담는 위치에 저장 Combine사용

--- LMessenger/Repository/DBError.swift
+++ LMessenger/Repository/DBError.swift
@@ -2,7 +2,7 @@
 //  DBError.swift
 //  LMessenger
 //
-//  Created by 박진성 on 2/11/24.
+//  Created by 박진성 on 2/12/24.
 //
 
 import Foundation
--- LMessenger/Service/UserService.swift
+++ LMessenger/Service/UserService.swift
@@ -2,7 +2,7 @@
 //  UserService.swift
 //  LMessenger
 //
-//  Created by 박진성 on 2/11/24.
+//  Created by 박진성 on 2/12/24.
 //
 
 import Foundation
